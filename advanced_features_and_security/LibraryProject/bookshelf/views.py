bb97358acaff058095a62ac7b6ec78d4806bb734
0xelochukwu <charleselochukwumagbo@gmail.com>
2025-11-11 23:50:29 +0100

    edit

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from .models import Book
 
-# Create your views here.
+# Permission check function
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+# View for listing books
+@user_passes_test(is_librarian, raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()  # Queryset variable 'books'
+    return render(request, 'bookshelf/book_list.html', {'books': books})
