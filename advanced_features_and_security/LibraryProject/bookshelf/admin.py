e6c42afe878dcbdfd845d211168933630fecc9b4
0xelochukwu <charleselochukwumagbo@gmail.com>
2025-11-11 17:28:40 +0100

    advanced

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, UserProfile, Author, Book, Library
+
+# Customize the admin interface for CustomUser
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'first_name', 'last_name', 'date_of_birth', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+# Register CustomUser
+admin.site.register(CustomUser, CustomUserAdmin)
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author')  # removed publication_year
-    list_filter = ('author',)           # removed publication_year
-    search_fields = ('title', 'author__name')  # author is a ForeignKey
+    list_display = ('title', 'author')
+    list_filter = ('author',)
+    search_fields = ('title', 'author__name')
+
+
+admin.site.register(UserProfile)
+admin.site.register(Author)
+admin.site.register(Library)
